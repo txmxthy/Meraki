93e2fa9a710b17dc0abe6c9ca8ece2259506b5eb
timothy <txmm@protonmail.com>
2021-09-16 23:12:52 +1200

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,39 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import MainMenuScreen from "./src/pages/main-menu";
-import LoginScreen from "./src/pages/login";
-import RadarScreen from "./src/pages/radar";
-import MatrixScreen from "./src/pages/matrix";
-import CalendarScreen from "./src/pages/calendar";
-import GlobalScreen from "./src/pages/global";
-import LateScreen from "./src/pages/overdue";
-
-const Tab = createBottomTabNavigator();
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Radar" component={RadarScreen} />
-        <Tab.Screen name="Matrix" component={MatrixScreen} />
-        <Tab.Screen name="Calendar" component={CalendarScreen} />
-        <Tab.Screen name="Global" component={GlobalScreen} />
-        <Tab.Screen name="Late" component={LateScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- README.md
+++
@@ -1,3 +0,0 @@
-# Meraki
-
-SWEN325 Assignment 2.
